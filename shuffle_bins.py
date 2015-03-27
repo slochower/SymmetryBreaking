6f2c827404d6e0ad4b44c275d8da6ef02ff701ff
David Slochower <slochower@gmail.com>
2015-03-27 10:53:43 -0700

    Brownian walker working on landscapes.

--- shuffle_bins.py
+++ shuffle_bins.py
@@ -2,13 +2,14 @@
 
 from math import *
 from numpy import *
+from scipy.stats import norm
 import matplotlib.cm as cm
 # from scipy import optimize
 
 execfile('../common/figures.py')
 
 debug = True
-animation, density, annotate = False, False, True
+animation, density, annotate = False, True, False
 
 
 def landscapes(x, shift=0):
@@ -23,11 +24,9 @@ def find_nearest(array, value):
     idx = (abs(array-value)).argmin()
     return array[idx]
 
-def force_at_position(x, dx):
-    energies = [landscapes(j) for j in [positions[k] for k in
-                range(len(positions))]]
-    force = -diff(energies) / dx
-    return force
+def force(x, dx):
+    return float(-(landscapes(x+dx, shift=shift) -
+                   landscapes(x, shift=shift)) / dx)
 
 
 #################################
@@ -47,6 +46,7 @@ s = []    # List of beginning points for moving on the energy landscapes
 for i in range(num_landscapes):
     if i % 2 == 0:
         j = 0.0
+        j = shift
     else:
         j = shift
     E.append([landscapes(i, shift=j) for i in q])
@@ -60,20 +60,19 @@ for i in range(num_landscapes):
     jumps = []
     fluxes = []
     x = pi     # Start Brownian walker at position x = pi
-    force = -diff(E[i]) / dx
+#    force = -diff(E[i]) / dx
     D = 0.1      # Arbitrary
 #################################
     kT = 100.0  # Arbitrary
 #################################
     positions.append(x)
-    timesteps = 10
     for t in range(timesteps):
         swap = 0
         gamma = random.normal(loc=0.0, scale=2*D*dt)
-#        new_x = x + (D/kT) * force[x] * dt + gamma
-        new_x = x + force[x] * dt
+        new_x = x + (D/kT) * force(x,dx) * dt + gamma
+#        new_x = x + force(x,dx) * dt
         if debug:
-            print 't = {} \t x = {}, force * dt = {}, new_x = {}'.format(float(t), float(x), float(force[x]*dt), float(new_x))
+            print 't = {} \t x = {}, force * dt = {}, new_x = {}'.format(float(t), float(x), float(force(x,dx)*dt), float(new_x))
 
         if new_x > max(q):
             print 'Wrapping {} to {}'.format(new_x, min(q)+(new_x-max(q)))
@@ -111,7 +110,7 @@ for i in range(num_landscapes):
 # PLOTTING
 ##################################
 
-c = 'r'
+c = 'b'
 fig, gs, axes = generate_axes_pad(nrows=3, ncols=2, v_pad=0.3,
                                   h_pad=0.2, figsize=(12, 12))
 ax = axes[0][0]
@@ -119,7 +118,7 @@ ax.plot(q, E[0], color=c, lw=2)
 ax.set_title('Energy landscape')
 
 ax = axes[0][1]
-ax.plot(q[1:], force, color=c, lw=2)
+ax.plot(q, [force(x, dx) for x in q], color=c, lw=2)
 ax.set_title('Force')
 
 ax = axes[1][0]
@@ -127,28 +126,28 @@ ax.plot(q, boltzmann, color='k', lw=2, label='kT = {}'.format(kT))
 ax.set_title('Boltzmann distribution')
 ax.legend()
 
-if annotate:
-    ax = axes[1][1]
-    ax.set_title('Energy landscape')
-    ax.plot(q, E[0], color='r', lw=2)
-    energies = [landscapes(j) for j in [positions[k] for k in
-                range(len(positions))]]
-    ax.scatter(positions, energies,
-               c='k',
-               s=200, lw=0, alpha=1)
-    labels = ['Point {0}'.format(i) for i in range(len(positions))]
-    for label, x, y in zip(labels, positions, energies):
-        plt.annotate(
-                     label,
-                     xy = (x, y), xytext = (-20, 20),
-                     textcoords = 'offset points', ha = 'right', 
-                     va = 'bottom',
-                     bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', 
-                     alpha = 0.5),
-                     arrowprops = dict(arrowstyle = '->', 
-                     connectionstyle = 'arc3,rad=0'))
-    ax.legend()
-
+ax = axes[1][1]
+ax.set_title('Position density along energy landscape')
+ax.plot(q, E[0], color=c, lw=2)
+energies = [landscapes(j,shift=shift) for j in [positions[k] for k in
+            range(len(positions))]]
+ax.scatter(positions, energies,
+           c='k',
+           s=200, lw=0, alpha=0.003)
+labels = ['Point {0}'.format(i) for i in range(len(positions))]
+'''
+for label, x, y in zip(labels, positions, energies):
+    plt.annotate(
+                 label,
+                 xy = (x, y), xytext = (-20, 20),
+                 textcoords = 'offset points', ha = 'right', 
+                 va = 'bottom',
+                 bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', 
+                 alpha = 0.5),
+                 arrowprops = dict(arrowstyle = '->', 
+                 connectionstyle = 'arc3,rad=0'))
+ax.legend()
+'''
 ax = axes[2][0]
 counts, edges = histogram(positions, range=(min(q), max(q)),
                           bins=10, normed=True)
@@ -164,7 +163,13 @@ counts, edges = histogram(jumps, range=(min(jumps), max(jumps)),
                           bins=10, normed=True)
 mids = (edges[1:]+edges[:-1])/2.
 ax.bar(mids, counts, color=c, width=mids[1]-mids[0])
-ax.set_title('Histogram of step sizes')
+mu, std = norm.fit(jumps)
+xmin, xmax = min(jumps), max(jumps)
+x = linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu, std)
+ax.plot(x, p, 'k', linewidth=2)
+ax.set_title('Step sizes: $\mu = {0:.4f}, \sigma = {0:.3f}$'.format(mu,std))
+ax.legend()
 plt.show()
 # plt.savefig('shuffle_bins_kT.{}.png'.format(float(kT)))
 plt.close(fig)
@@ -177,7 +182,7 @@ if animation:
         cmap = cm.jet
         c = linspace(min(positions), max(positions), len(positions))
         ax.set_title('Energy landscape')
-        ax.plot(q, E[0], color='r', lw=2)
+        ax.plot(q, E[0], color=c, lw=2)
         cax = ax.scatter(positions[k],
                          E[0][where(q == find_nearest(q, positions[k]))[0][0]],
                          #c=c[k], cmap=cmap,
@@ -191,20 +196,20 @@ if animation:
         plt.savefig('tmp.{:02d}.png'.format(k))
         plt.close(fig)
     from subprocess import call
-    call(["ffmpeg", "-framerate", "4", "-i", "tmp.%02d.png", "-c:v",
+    call(["ffmpeg", "-framerate", "90", "-i", "tmp.%02d.png", "-c:v",
           "libx264", "-r", "30", "-pix_fmt", "yuv420p", "motion.mp4"])
     call(["rm", "tmp*.png"])
     import glob
     import os
-    for fl in glob.glob("tmp*.png"):
-        os.remove(fl)
+#    for fl in glob.glob("tmp*.png"):
+#        os.remove(fl)
 
 if density: 
     fig, gs, axes = generate_axes_pad(nrows=1, ncols=1, v_pad=0.3,
                                       h_pad=0.2, figsize=(12, 12))
     ax = axes[0][0]
     ax.set_title('Energy landscape')
-    ax.plot(q, E[0], color='r', lw=2)
+    ax.plot(q, E[0], color=c, lw=2)
     ax.scatter(positions, [-15 for k in range(len(positions))],
                          c='k',
                          s=200, lw=0, alpha=0.005,
@@ -220,7 +225,7 @@ if annotate:
                                       h_pad=0.2, figsize=(12, 12))
     ax = axes[0][0]
     ax.set_title('Energy landscape')
-    ax.plot(q, E[0], color='r', lw=2)
+    ax.plot(q, E[0], color=c, lw=2)
     energies = [landscapes(j) for j in [positions[k] for k in
                 range(len(positions))]]
     ax.scatter(positions, energies,
